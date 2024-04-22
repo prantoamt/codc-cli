f36c6811f09ff164b80ee3d47f73f66bad38c0cb
Md Badiuzzaman Pranto <prantoamt@gmail.com>
2024-04-22 16:06:27 +0200

    pdm added

--- dc_copula_cli.py
+++ dc_copula_cli.py
@@ -11,19 +11,19 @@ from copula.empirical_copula import EmpiricalCopula
     "--inputfile_1",
     type=str,
     required=True,
-    help="Path to the TSV file containing the tumor gene expression data.",
+    help="Path to the TSV file containing gene expression data.",
 )
 @click.option(
     "--inputfile_2",
     type=str,
     required=True,
-    help="Path to the TSV file containing the normal gene expression data.",
+    help="Path to the TSV file containing gene expression data.",
 )
 @click.option(
     "--output_path",
     type=str,
     required=True,
-    help="Output path to save the resulting TSV file containing the distance matrix. The output file name will be network.tsv",
+    help="Output path to save the resulting TSV file containing the differential coexpression network. The file name will be network.tsv",
 )
 @click.option(
     "--ties_method",
@@ -41,20 +41,25 @@ from copula.empirical_copula import EmpiricalCopula
     "--ks_stat_method",
     type=click.Choice(["asymp", "auto", "exact"]),
     default="asymp",
-    help="Mode parameter for the ks_2samp function.",
+    help="Mode parameter for the ks_2samp function, which determines how the Kolmogorov-Smirnov statistic is computed.",
 )
 def main(inputfile_1, inputfile_2, output_path, ties_method, smoothing, ks_stat_method):
     """
     This script computes a matrix of differential coexpression scores for gene pairs across two conditions,
-    using the Kolmogorov-Smirnov distance between their empirical copulas. It assesses the similarity
-    in gene expression distributions between phenotypes such as 'tumor' and 'normal'.
+    using the Kolmogorov-Smirnov distance between their empirical copulas (Differential Empirical Copula). It assesses the similarity
+    in gene expression distributions between phenotypes such as 'tumor' and 'normal' (Could be other as well). It then creates a differential
+    coexpression matrix from the differential Empirical Copula and stores the matrix as a network.tsv file in the
+    specified output directory.
     """
+    # Loading data from TSV files
     df1 = pd.read_csv(inputfile_1, delimiter="\t")
     df2 = pd.read_csv(inputfile_2, delimiter="\t")
 
+    # Initializing the EmpiricalCopula and GeneExpressionAnalyzer instances
     empirical_copula = EmpiricalCopula()
     analyzer = GeneExpressionAnalyzer(empirical_copula=empirical_copula)
 
+    # Computing the distance matrix using the specified methods
     distance_matrix = analyzer.compute_dc_copula_matrix(
         df1,
         df2,
@@ -63,6 +68,8 @@ def main(inputfile_1, inputfile_2, output_path, ties_method, smoothing, ks_stat_
         ks_stat_method=ks_stat_method,
     )
 
+    # Saving the distance matrix to the specified output path
+    output_path = f"{output_path}/network.tsv"
     np.savetxt(output_path, distance_matrix, delimiter="\t", fmt="%f")
     print(f"Saved the computed distance matrix to {output_path}")
 
